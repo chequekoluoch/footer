e1ee44738fd44f134d27701fbe5b840be43566e1
makombekepha <makombekepha@gmail.com>
2020-10-21 16:33:48 +0300

    footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Footer from './Footer.js'
+
+function App() {
+  return (
+    <Router>
+      <Footer />
+      </Router>
+    
+  );
+}
+
+export default App;
